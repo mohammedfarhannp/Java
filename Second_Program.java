ff17c9db7ce37c8a4b14efc4a284ce1a381bc29d
Mohammed Farhan NP <65911037+mohammedfarhannp@users.noreply.github.com>
2024-12-12 22:36:07 +0530

    Update Second_Program.java
    Comments Added!

--- Second_Program.java
+++ Second_Program.java
@@ -1,13 +1,19 @@
 // Calculator
+// For user input scanner module or class or something is imported
 import java.util.Scanner;
 
+// Main class
 class Main {
+    // Main Function
     public static void main(String[] args)
     {
+        // Scanner type variable scanner is declared and is instance of class Scanner
         Scanner scanner = new Scanner(System.in); // For Input from user
-        
+
+        // Floating Number
         float result = 0.0f; // f to avoid ambiguity of compiler for it take it as double sometimes.
 
+        // Print function to avoid printing \n at the end unlike println
         System.out.print("Enter First Number: ");
         int A = scanner.nextInt();
 
@@ -17,8 +23,10 @@ class Main {
         System.out.print("\n1 - Add\n2 - Subtract\n3 - Multiply\n4 - Divide\n# ");
         int C = scanner.nextInt();
 
+        // Closing the Scanner class type scanner descriptor or something
         scanner.close();
 
+        // Switch case like in C
         switch (C)
         {
             case 1:
@@ -37,29 +45,37 @@ class Main {
                 System.out.println("Invalid Choice\n");
         }
 
+        // String Concatenation "A" + "B" or "A" + String type variable 
         System.out.println("Result : " + result + "\n");
     }
 
+
+    // Public function Sum
     public static int Sum(int x, int y)
     {
         return x + y;
     }
 
+    // Public function Sub for Subtracting
     public static int Sub(int x, int y)
     {
         return x - y;
     }
 
+    // Public function Mul for Multiplying
     public static int Mul(int x, int y)
     {
         return x * y;
     }
 
+    // Public function Div for Dividing
     public static float Div(int x, int y)
     {
+        // floating point number
         float result = 0.0f;
         try
         {
+            // Attempting to do the belo operation
             result = x / (float) y;
         } catch(ArithmeticException error)
         {
