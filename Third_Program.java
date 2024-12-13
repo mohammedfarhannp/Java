90af6ec19106350278e32aaf5044f41300284097
Mohammed Farhan NP <65911037+mohammedfarhannp@users.noreply.github.com>
2024-12-13 06:56:44 +0530

    Update Third_Program.java
    Comments Added!

--- Third_Program.java
+++ Third_Program.java
@@ -1,49 +1,58 @@
 // Multi Class Program
-
+// For getting user input I import something called Scanner
 import java.util.Scanner;
 
+// Class Calculator
 class Calculator {
+    // Function for addition
     public static int Sum(int x, int y)
     {
         return x + y;
     }
 
+    // Function for Subtraction
     public static int Subtract(int x, int y)
     {
         return x - y;
     }
 
+    // Function for Multiplication
     public static int Multiply(int x, int y)
     {
         return x * y;
     }
 
+    // Function for Division
     public static float Divide(int x, int y)
     {
         float result = 0.0f;
+
+        // If Denominator is zero
         if(y == 0)
         {
             System.out.println("ZeroDivisionError");
-        } else {
+        } else { // Else Divide x by y
             result = x / (float) y;
         }
         return result;
     }
 }
 
+// Main class
 class Main
 {
+    // main function of Main class
     public static void main(String[] args)
     {
-        Calculator Calc = new Calculator();
-        Scanner scan = new Scanner(System.in);
+        Calculator Calc = new Calculator(); // New Calculator Object stored in datatype Calculator
+        Scanner scan = new Scanner(System.in); // Scanner for getting user input
 
         int A, B, Oper;
 
         float Result = 0.0f;
 
         System.out.print("Enter first number: ");
-        A = scan.nextInt();
+        A = scan.nextInt(); // Gets the integer input from user.
 
         System.out.print("Enter second number: ");
         B = scan.nextInt();
