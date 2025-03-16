4cad5b7eaffb815710e96a507e784e620c54cf92
Mohammed Farhan N P <m.farhan.np@gmail.com>
2025-03-16 22:31:58 +0530

    Update!

---
+++ if_else.java
@@ -0,0 +1,11 @@
+public class if_else {
+    public static void main(String args[])
+    {
+        int x = 10;
+        if (x == 10) {
+            System.out.println("Value of X is 10");
+        } else {
+            System.out.println("Value of X is not 10");
+        }
+    }
+}
